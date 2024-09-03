be15deba3657473719b9e2f33f487bfc13edfb86
Esraa234esraa <125605121+Esraa234esraa@users.noreply.github.com>
2024-09-04 01:45:19 +0300

    Update Layout.jsx

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -14,7 +14,7 @@ export default function Layout() {
     return (
         <div className='parent'>
             <Navbar />
-            <button onClick={toggleDarkMode} style={{ display: 'flex', fontSize: '24px', margin: '10px',width:'100px' }}>
+            <button onClick={toggleDarkMode} style={{ display: 'flex', fontSize: '24px', margin: '10px',width:'50px' }}>
                 {isDarkMode ? (
                     <i className='fa fa-sun'></i>
                 ) : (
